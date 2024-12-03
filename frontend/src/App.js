bb11de46cf0cabe73ddbedd7f2db5345db2466ad
Your Name <your_email@example.com>
2024-12-02 23:22:31 -0500

    add home and search page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,22 +5,27 @@ import SignUpStep1 from "./components/SignUpStep1";
 import SignUpStep2 from "./components/SignUpStep2";
 import ForgotPassword from "./components/ForgotPassword";
 import Home from "./components/Home";
+import Search from "./components/search/search";
+import MovieDetail from "./components/movie/MovieDetail";
 import "./App.css";
 
 const App = () => {
   return (
     <div className="page-background">
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup/step1" element={<SignUpStep1 />} />
-        <Route path="/signup/step2" element={<SignUpStep2 />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="*" element={<Login />} />
-      </Routes>
-    </Router>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup/step1" element={<SignUpStep1 />} />
+          <Route path="/signup/step2" element={<SignUpStep2 />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/movie/:id" element={<MovieDetail />} />
+          <Route path="*" element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   );
 };
+
 export default App;
